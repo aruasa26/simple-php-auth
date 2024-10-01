759641907102c3b0a7bca27f376a9a01c23ce5d5
Ubuntu <fisaboke@vm-server-admin.bpneuhowar1ezc1jlsrwrj2flh.jnbx.internal.cloudapp.net>
2024-10-01 13:53:04 +0000

    Update register.php

--- register.php
+++ register.php
@@ -20,7 +20,7 @@ session_start();
         <div class="font-sans text-gray-900 antialiased">
             <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#f8f4f3]">
                 <div>
-                    <a href="/"></a>
+                    <a href="/register.php"></a>
                         <h2 class="font-bold text-3xl">CREATE <span class="bg-[#f84525] text-white px-2 rounded-md">ACCOUNT</span></h2>
                     </a>
                 </div>
